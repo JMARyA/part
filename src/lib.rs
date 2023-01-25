2177d94e135592bc6038301d23e4aae18ad7b125
JMARyA <jmarya@hydrar.de>
2023-01-25 11:14:14 +0100

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Read;
-use std::io::Write;
 use std::io::Seek;
-use serde::{Deserialize, Serialize};
+use std::io::Write;
 
 pub enum SplitOptions {
     NumberOfParts(usize),
-    SizeOfParts(usize)
+    SizeOfParts(usize),
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -14,7 +14,7 @@ pub struct SplitInfo {
     filename: String,
     number_of_parts: usize,
     hash: u32,
-    part_hashes: Vec<u32>
+    part_hashes: Vec<u32>,
 }
 
 pub fn split_file(f: &str, options: SplitOptions) {
@@ -26,31 +26,37 @@ pub fn split_file(f: &str, options: SplitOptions) {
     match options {
         SplitOptions::NumberOfParts(n) => {
             let nsize = fsize / n;
-            println!("Splitting file in {} parts with size {}B", n, nsize);
+            println!("Splitting file in {n} parts with size {nsize}B");
             make_split_files(&mut file, content, n, nsize, f);
         }
         SplitOptions::SizeOfParts(s) => {
             let n = fsize / s;
-            println!("Splitting file in {} parts with size {}B", n, s);
+            println!("Splitting file in {n} parts with size {s}B");
             make_split_files(&mut file, content, n, s, f);
         }
     }
 }
 
 fn make_split_files(file: &mut File, content: Vec<u8>, n: usize, s: usize, f: &str) {
-    let mut info = SplitInfo{
-        filename: String::from(std::path::Path::new(&f).file_name().unwrap().to_str().unwrap()),
+    let mut info = SplitInfo {
+        filename: String::from(
+            std::path::Path::new(&f)
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap(),
+        ),
         hash: crc32fast::hash(&content),
         number_of_parts: n,
-        part_hashes: vec![]
+        part_hashes: vec![],
     };
 
     for i in 0..n {
-        println!("Writing Part {}", i);
+        println!("Writing Part {i}");
         let mut part = File::create(format!("{}.{}.part", &f, i)).unwrap();
-        let mut read_buf: Vec<u8> = vec![0u8; s as usize];
-        file.seek(std::io::SeekFrom::Start((s*i) as u64)).unwrap();
-        if i == n-1 {
+        let mut read_buf: Vec<u8> = vec![0u8; s];
+        file.seek(std::io::SeekFrom::Start((s * i) as u64)).unwrap();
+        if i == n - 1 {
             read_buf = vec![];
             file.read_to_end(&mut read_buf).unwrap();
         } else {
@@ -62,31 +68,35 @@ fn make_split_files(file: &mut File, content: Vec<u8>, n: usize, s: usize, f: &s
         info.part_hashes.push(phash);
 
         // WRITE FILE
-        part.write_all(&mut read_buf).unwrap();
+        part.write_all(&read_buf).unwrap();
     }
 
     let mut info_f = File::create(format!("{}.partinfo", &f)).unwrap();
-    let mut info_s = serde_json::to_vec(&info).unwrap();
-    info_f.write_all(&mut info_s).unwrap();
+    let info_s = serde_json::to_vec(&info).unwrap();
+    info_f.write_all(&info_s).unwrap();
 }
 
 pub fn combine_file(f: &str) {
     let mut info_c = vec![];
-    File::open(&f).unwrap().read_to_end(&mut info_c).unwrap();
+    File::open(f).unwrap().read_to_end(&mut info_c).unwrap();
 
     let info: SplitInfo = serde_json::from_slice(&info_c).unwrap();
-    let basename = std::path::Path::new(&f).file_stem().unwrap().to_str().unwrap();
+    let basename = std::path::Path::new(&f)
+        .file_stem()
+        .unwrap()
+        .to_str()
+        .unwrap();
 
     let mut final_content: Vec<u8> = vec![];
 
     for i in 0..info.number_of_parts {
         let mut read = vec![];
-        let mut part_f = File::open(format!("{}.{}.part", basename, i)).unwrap();
+        let mut part_f = File::open(format!("{basename}.{i}.part")).unwrap();
         part_f.read_to_end(&mut read).unwrap();
 
         // CHECKSUM TEST
         if crc32fast::hash(&read) != *info.part_hashes.get(i).unwrap() {
-            println!("Checksum error on part {}", i);
+            println!("Checksum error on part {i}");
             std::process::exit(1);
         }
 
@@ -96,7 +106,7 @@ pub fn combine_file(f: &str) {
         println!("Checksum error on completed file");
         std::process::exit(1);
     }
-    println!("File successfile combined");
+    println!("File successfully combined");
     let mut ff = File::create(basename).unwrap();
-    ff.write_all(&mut final_content).unwrap();
+    ff.write_all(&final_content).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,41 @@ use clap::{App, Arg};
 
 fn main() {
     let args = App::new("part")
-        .version(option_env!("CARGO_PKG_VERSION").unwrap())
+        .version(env!("CARGO_PKG_VERSION"))
         .author("JMARyA <jmarya0@icloud.com>")
         .about("split and combine files")
         .multicall(true)
         .subcommand(
             App::new("split")
-            .alias("s")
-            .about("split into multiple parts")
-            .arg(
-                Arg::with_name("parts")
-                    .short('n')
-                    .long("number")
-                    .required(false)
-                    .value_name("PARTS")
-                    .conflicts_with("size")
-                    .help("number of part files"),
-            )
-            .arg(
-                Arg::with_name("size")
-                    .short('s')
-                    .long("size")
-                    .required(false)
-                    .value_name("SIZE")
-                    .conflicts_with("parts")
-                    .help("size of individual part files"),
-            )
-            .arg(
-                Arg::with_name("file")
-                    .required(true)
-                    .value_name("FILE")
-                    .help("file to split"),
-            ))
-            .subcommand(
-                App::new("combine")
+                .alias("s")
+                .about("split into multiple parts")
+                .arg(
+                    Arg::with_name("parts")
+                        .short('n')
+                        .long("number")
+                        .required(false)
+                        .value_name("PARTS")
+                        .conflicts_with("size")
+                        .help("number of part files"),
+                )
+                .arg(
+                    Arg::with_name("size")
+                        .short('s')
+                        .long("size")
+                        .required(false)
+                        .value_name("SIZE")
+                        .conflicts_with("parts")
+                        .help("size of individual part files"),
+                )
+                .arg(
+                    Arg::with_name("file")
+                        .required(true)
+                        .value_name("FILE")
+                        .help("file to split"),
+                ),
+        )
+        .subcommand(
+            App::new("combine")
                 .about("combine multiple parts")
                 .alias("c")
                 .arg(
@@ -43,8 +44,8 @@ fn main() {
                         .required(true)
                         .value_name("FILE")
                         .help("file partinfo"),
-                )
-            )
+                ),
+        )
         .get_matches();
 
     match args.subcommand().unwrap() {
@@ -53,11 +54,11 @@ fn main() {
             let parts = args.value_of("parts");
             let size = args.value_of("size");
 
-            if parts.is_some() {
-                part::split_file(f, part::SplitOptions::NumberOfParts(parts.unwrap().parse().unwrap()));
+            if let Some(parts) = parts {
+                part::split_file(f, part::SplitOptions::NumberOfParts(parts.parse().unwrap()));
             }
-            if size.is_some() {
-                part::split_file(f, part::SplitOptions::SizeOfParts(size.unwrap().parse().unwrap()));
+            if let Some(s) = size {
+                part::split_file(f, part::SplitOptions::SizeOfParts(s.parse().unwrap()));
             }
         }
         ("combine", args) => {
